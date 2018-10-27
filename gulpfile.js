29bd05c79521a5c6282668aae1c0d3f19bc2ac16
DavideTriso <davidetrisolinI@yahoo.it>
2018-10-27 22:07:16 +0200

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,66 @@
 //Require packages
 var gulp = require('gulp'),
-  sass = require('gulp-ruby-sass'),
+  sass = require('gulp-sass'),
   autoprefixer = require('gulp-autoprefixer'),
   cssnano = require('gulp-cssnano'),
-  uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
-  concat = require('gulp-concat'),
   notify = require('gulp-notify');
+sass.compiler = require('node-sass');
+
 
 //SCSS -> CSS
 //Compile SCSS and save compiled + minified CSS file to 'dist' folder
-gulp.task('scssDist', function (done) {
-  sass('src/scss/*.scss', { style: 'expanded' }).pipe(autoprefixer('last 5 version')).pipe(gulp.dest('dist')).pipe(rename({ suffix: '.min' })).pipe(cssnano()).pipe(gulp.dest('dist')).pipe(notify({ message: 'scssDist task complete' }));
-  done();
+gulp.task('scssDist', function () {
+  sass('src/scss/*.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 5 version'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({ message: 'scssDist task complete' }));
 });
 
 //Save a copy of compiled CSS in 'docs' folder
-gulp.task('scssDocs', function (done) {
-  sass('src/scss/*.scss', { style: 'expanded' }).pipe(autoprefixer('last 5 version')).pipe(gulp.dest('docs')).pipe(rename({ suffix: '.min' })).pipe(notify({ message: 'scssDocs task complete' }));
-  done();
+gulp.task('scssDocs', function () {
+  sass('src/scss/*.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 5 version'))
+    .pipe(gulp.dest('docs'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(notify({ message: 'scssDocs task complete' }));
 });
-
-//Task alias
-gulp.task('scss', gulp.parallel('scssDist', 'scssDocs'));
 
 //JS -> JS + MIN.JS
 //Put a copy and a minified version of JS file in 'dist' folder
-gulp.task('jsDist', function (done) {
-  gulp.src('src/js/*.js').pipe(gulp.dest('dist')).pipe(rename({ suffix: '.min' })).pipe(uglify()).pipe(gulp.dest('dist')).pipe(notify({ message: 'jsDist task complete' }));
-  done();
+gulp.task('jsDist', function () {
+  gulp.src('src/js/*.js')
+    .pipe(gulp.dest('dist'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({ message: 'jsDist task complete' }));
 });
 
 //Put a copy of JS file in 'docs' folder
-gulp.task('jsDocs', function (done) {
-  gulp.src('src/js/*.js').pipe(gulp.dest('docs')).pipe(notify({ message: 'jsDocs task complete' }));
-  done();
+gulp.task('jsDocs', function () {
+  gulp.src('src/js/*.js')
+    .pipe(gulp.dest('docs'))
+    .pipe(notify({ message: 'jsDocs task complete' }));
 });
 
-//Task alias
-gulp.task('js', gulp.parallel('jsDist', 'jsDocs'));
 
 //HTML -> HTML
 //Put a copy of HTML file to 'dist' folder
-gulp.task('htmlDist', function (done) {
-  gulp.src('src/html/*.html').pipe(gulp.dest('dist')).pipe(notify({ message: 'htmlDist task complete' }));
-  done();
+gulp.task('htmlDist', function () {
+  gulp.src('src/html/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({ message: 'htmlDist task complete' }));
 });
 
-gulp.task('html', gulp.parallel('htmlDist'));
 
 //WATCH TASKS
 
-gulp.task('watchScss', function () {
-  gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'));
-});
-
-gulp.task('watchJS', function () {
-  gulp.watch('src/js/*.js', gulp.parallel('js'));
-});
-
-gulp.task('watchHTML', function () {
-  gulp.watch('src/html/*.html', gulp.parallel('html'));
-});
-
 gulp.task('watchAll', function () {
-  gulp.watch('src/js/*.js', gulp.parallel('js'));
-  gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('src/html/*.html', gulp.parallel('html'));
+  gulp.watch('src/js/*.js', ['jsDist', 'jsDocs']);
+  gulp.watch('src/scss/**/*.scss', ['scssDist', 'scssDocs']);
+  gulp.watch('src/html/*.html', ['htmlDist']);
 });
